23bd90605c866a816331b056ecce836c62956c0e
chyacin <calvinhyacinth@gmail.com>
2021-01-09 11:45:09 +0100

    Add the Timezone=UTC to the server

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
@@ -1,12 +1,21 @@
 package com.nnk.springboot;
 
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableWebSecurity
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
 }
