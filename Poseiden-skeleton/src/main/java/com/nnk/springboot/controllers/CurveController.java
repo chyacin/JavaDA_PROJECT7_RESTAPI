4514908f2d7587c39e3e634d97b32d7eb5b03c34
chyacin <calvinhyacinth@gmail.com>
2021-01-17 15:36:13 +0100

    Completed the CurveController ToDo list

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -1,6 +1,14 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.services.CurveServiceImpl;
+import com.nnk.springboot.services.UserService;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,45 +18,165 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CurveController {
     // TODO: Inject Curve Point service
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    CurveServiceImpl curvePointService;
+
+    Logger log = LoggerFactory.getLogger(CurveController.class);
+    private final List<CurvePoint> curvePointList = new ArrayList<>();
+
+    /**
+     * The controller method which gets the home page with all the curve points
+     * The user needs to be logged in
+     * @param username logged in user details(information)
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url with all the curve points
+     */
     @RequestMapping("/curvePoint/list")
-    public String home(Model model)
+    public String home(@AuthenticationPrincipal UserDetails username, Model model)
     {
         // TODO: find all Curve Point, add to model
+        String loggedInUsername = username.getUsername(); // get logged in username
+        User loggedInUser = userService.findUserByUsername(loggedInUsername);
+
+        if(loggedInUser != null){
+            model.addAttribute("curvePoint", curvePointService.findAllCurvePoint());
+        }
+
+        log.info("Total Curve Points in a List" + curvePointList.size());
+
         return "curvePoint/list";
     }
 
+    /**
+     * The controller method which gets the form where the user can add their curve points
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param bid the object that contains the details of the curve point
+     * @return the url where you can find Curve Point form
+     * */
     @GetMapping("/curvePoint/add")
-    public String addBidForm(CurvePoint bid) {
+    public String addBidForm(@AuthenticationPrincipal UserDetails username, CurvePoint bid) {
         return "curvePoint/add";
     }
 
+    /**
+     * The controller method which check if the data is valid and then save it to the database
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param curvePoint the object that contains the details of the curve point
+     * @param result the validation status of each input field in the form
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url which redirects and returns the saved curve point
+     */
     @PostMapping("/curvePoint/validate")
-    public String validate(@Valid CurvePoint curvePoint, BindingResult result, Model model) {
+    public String validate(@AuthenticationPrincipal UserDetails username, @Valid CurvePoint curvePoint, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Curve list
-        return "curvePoint/add";
+
+        if(result.hasErrors()){
+            log.info("errors: " + result.getAllErrors());
+            return "curvePoint/add";
+        }
+        curvePoint.setCreationDate(Timestamp.from(Instant.now()));
+        curvePointService.saveCurvePoint(curvePoint);
+        log.info("Curve Id" + curvePoint.getCurveId(), "Term" + curvePoint.getTerm(),
+                "Value" + curvePoint.getValue());
+
+        return"redirect:/curvePoint/list";
     }
 
+    /**
+     * The controller method which gets the form where the user can update a curve point with new information
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param id the integer id of each bid which helps the user identify each bid made
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url which redirects and returns the web page with form to be updated
+     */
     @GetMapping("/curvePoint/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
+    public String showUpdateForm(@AuthenticationPrincipal UserDetails username, @PathVariable("id") Integer id, Model model) {
         // TODO: get CurvePoint by Id and to model then show to the form
-        return "curvePoint/update";
+
+        CurvePoint curvePoint = curvePointService.findCurvePointById(id);
+
+        if(curvePoint != null){
+          model.addAttribute("curvePoint", curvePoint);
+            log.info("Curve Id" + curvePoint.getCurveId(), "Term" + curvePoint.getTerm(),
+                    "Value" + curvePoint.getValue());
+
+            return "curvePoint/update";
+
+        }
+        return"redirect:/curvePoint/list";
     }
 
+    /**
+     * The controller method which processes the form where the user can update a curve point with new information
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param id the integer id of each curve point which helps the user identify each curve point made
+     * @param curvePoint the object that contains the details of the curve point
+     * @param result the validation status of each input field in the form
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url which redirects and returns the updated curve point in the curve list
+     */
     @PostMapping("/curvePoint/update/{id}")
-    public String updateBid(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
+    public String updateBid(@AuthenticationPrincipal UserDetails username, @PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Curve and return Curve list
+
+        if(result.hasErrors()) {
+            log.info("curve errors" + result.getAllErrors());
+            return "/curvePoint/update";
+        }
+        curvePoint.setId(id);
+        curvePointService.updateCurvePoint(curvePoint);
+
+        log.info("Updated Curve Point" + curvePoint.toString(),
+                "Updated Curve Point time" + Timestamp.from(Instant.now()));
+
         return "redirect:/curvePoint/list";
     }
 
+    /**
+     * The controller method which gets the option where the user can delete a curve point
+     * The user needs to be logged in
+     * @param username logged in user details(information)
+     * @param id the integer id of each bid which helps the user identify each curve point made
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url which redirects and returns the bid list without the bid that was deleted
+     */
     @GetMapping("/curvePoint/delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
+    public String deleteBid(@AuthenticationPrincipal UserDetails username, @PathVariable("id") Integer id, Model model) {
         // TODO: Find Curve by Id and delete the Curve, return to Curve list
-        return "redirect:/curvePoint/list";
+
+        CurvePoint curvePointById = curvePointService.findCurvePointById(id);
+
+        if(curvePointById != null) {
+            System.out.println("curve point to delete" + id);
+            try {
+                curvePointService.deleteCurvePoint(id);
+            }catch (Exception e){
+                System.out.println("error with deleting curve point");
+            }
+            log.info("Deleted Curve point" + curvePointById.toString());
+            return "redirect:/curvePoint/list";
+        }
+        return "curvePoint/list";
     }
 }
