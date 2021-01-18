3f817ec23dc1c29327af3f91e49ec001dfae7f0c
chyacin <calvinhyacinth@gmail.com>
2021-01-18 16:25:29 +0100

    Add Javadocs to the Home and Login Controllers

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.nnk.springboot.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController
 {
+
+	/**
+	 * The controller method which gets the user their home page
+	 * The user needs to be logged in
+	 * If the user isn't logged in, they will be redirected to the login page
+	 * @param username logged in user details(information)
+	 * @param model a request scoped object injected for us by spring and it's stores attributes.
+	 * @return the url where the user can find the home page
+	 */
 	@RequestMapping("/")
-	public String home(Model model)
+	public String home(@AuthenticationPrincipal UserDetails username, Model model)
 	{
 		return "home";
 	}
 
+
+	/**
+	 * The controller method which gets the admin their home page
+	 * The user needs to be logged in
+	 * If the user isn't logged in, they will be redirected to the login page
+	 * @param username logged in user details(information)
+	 * @param model a request scoped object injected for us by spring and it's stores attributes.
+	 * @return the url which redirects and returns the bidList in a list
+	 */
 	@RequestMapping("/admin/home")
-	public String adminHome(Model model)
+	public String adminHome(@AuthenticationPrincipal UserDetails username, Model model)
 	{
 		return "redirect:/bidList/list";
 	}
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -2,6 +2,8 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,10 @@ public class LoginController {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * The controller method which gets the user the login page
+     * @return the login page
+     */
     @GetMapping("login")
     public ModelAndView login() {
         ModelAndView mav = new ModelAndView();
@@ -21,16 +27,36 @@ public class LoginController {
         return mav;
     }
 
-    @GetMapping("secure/article-details")
-    public ModelAndView getAllUserArticles() {
+    /**
+     * The controller method where the admin can view, edit or delete a user
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @return the url returns the list of users for this application
+     */
+    @GetMapping("app/secure/article-details")
+    public ModelAndView getAllUserArticles(@AuthenticationPrincipal UserDetails username) {
         ModelAndView mav = new ModelAndView();
-        mav.addObject("users", userRepository.findAll());
-        mav.setViewName("user/list");
+        if(username != null && username.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
+            mav.addObject("users", userRepository.findAll());
+            mav.setViewName("user/list");
+        }
+        else{
+            mav.setViewName("403");
+        }
         return mav;
     }
 
-    @GetMapping("error")
-    public ModelAndView error() {
+
+    /**
+     * The controller method which denies an unauthorized user access to a page where only authorized admin are allowed to access
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @return the url returns a page with information stating that the user doesn't have access
+     */
+    @GetMapping("app/error")
+    public ModelAndView error(@AuthenticationPrincipal UserDetails username) {
         ModelAndView mav = new ModelAndView();
         String errorMessage= "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
