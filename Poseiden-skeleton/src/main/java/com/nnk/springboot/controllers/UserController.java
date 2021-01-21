863d55a4492b4c10fd8b09d6d4c4059473360bfe
chyacin <calvinhyacinth@gmail.com>
2021-01-21 16:03:20 +0100

    Made minor changes and added the javadocs

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.config.ValidPassword;
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
+import com.nnk.springboot.services.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
@@ -15,68 +21,178 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 @Controller
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    Logger log = LoggerFactory.getLogger(UserController.class);
+    private final List<User> userList = new ArrayList<>();
+
+
+    /**
+     * The controller method which gets the home page with all the users
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url with all the users
+     */
     @RequestMapping("/user/list")
-    public String home(Model model)
+    public String home(@AuthenticationPrincipal UserDetails username, Model model)
     {
-        model.addAttribute("users", userRepository.findAll());
+        String loggedInUsername = username.getUsername(); // get logged in username
+        User loggedInUser = userService.findUserByUsername(loggedInUsername);
+
+        if(loggedInUser != null){
+            model.addAttribute("users", userService.findAllUsers());
+        }
+
+        log.info("Total Users in List: " + userList.size());
+
         return "user/list";
     }
 
+    /**
+     * The controller method which gets the form where the admin can add a new user
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param bid the object that contains the details of the user
+     * @return the url where you can find the user form
+     */
     @GetMapping("/user/add")
-    public String addUser(User bid) {
+    public String addUser(@AuthenticationPrincipal UserDetails username, User bid) {
         return "user/add";
     }
 
+    /**
+     * The controller method which check if the data is valid and then save it to the database
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param user the object that contains the details of the user
+     * @param result the validation status of each input field in the form
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url which redirects and returns the saved user
+     */
+    @RolesAllowed("ADMIN")
     @PostMapping("/user/validate")
-    public String validate(@Valid User user, BindingResult result, Model model) {
-        if (!result.hasErrors()) {
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            user.setPassword(encoder.encode(user.getPassword()));
-            userRepository.save(user);
-            model.addAttribute("users", userRepository.findAll());
-            return "redirect:/user/list";
+    public String validate(@AuthenticationPrincipal UserDetails username, @Valid User user, BindingResult result, Model model) {
+
+        if(result.hasErrors()){
+            log.info("User errors: "  + result.getAllErrors());
+            return "user/add";
         }
-        return "user/add";
+
+      /*  BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        user.setPassword(encoder.encode(user.getPassword()));*/
+
+        userService.saveUser(user);
+        model.addAttribute("users", userService.findAllUsers());
+
+        log.info("Full name: " + user.getFullname(), "Username: " + user.getUsername(),
+                "Role name: " + user.getRole());
+
+        return "redirect:/user/list";
+
     }
 
+    /**
+     * The controller method which gets the form where the admin can update a user with new information
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param id the integer id of each user
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url which redirects and returns the web page with form to be updated
+     */
     @GetMapping("/user/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        user.setPassword("");
-        model.addAttribute("user", user);
-        return "user/update";
+    public String showUpdateForm(@AuthenticationPrincipal UserDetails username, @PathVariable("id") Integer id, Model model) {
+
+        User user = userService.findUserById(id);
+
+        if(user != null){
+            user.setPassword("");
+            model.addAttribute("user", user);
+            log.info("Full name: " + user.getFullname(), "Username: " + user.getUsername(),
+                    "Role name: " + user.getRole());
+            return "user/update";
+        }
+        return "redirect:/user/list";
     }
 
+    /**
+     * The controller method which processes the form where the admin can update a user with new information
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param id the integer id of each user
+     * @param user the object that contains the details of the user
+     * @param result the validation status of each input field in the form
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url which redirects and returns the updated user in the user list
+     */
     @PostMapping("/user/update/{id}")
-    public String updateUser(@PathVariable("id") Integer id, @Valid User user,
+    public String updateUser(@AuthenticationPrincipal UserDetails username, @PathVariable("id") Integer id, @Valid User user,
                              BindingResult result, Model model) {
         if (result.hasErrors()) {
+            log.info("User errors: " + result.getAllErrors());
             return "user/update";
         }
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setPassword(encoder.encode(user.getPassword()));
         user.setId(id);
-        userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
+        userService.updateUser(user);
+        model.addAttribute("users", userService.findAllUsers());
+
+        log.info("Updated User: " + user.toString(),
+                  "Updated time: " + Timestamp.from(Instant.now()));
         return "redirect:/user/list";
     }
 
+    /**
+     * The controller method which gets the option where the admin can delete user
+     * The user needs to be logged in
+     * If the user isn't logged in, they will be redirected to the login page
+     * @param username logged in user details(information)
+     * @param id the integer id of each user
+     * @param model a request scoped object injected for us by spring and it's stores attributes.
+     * @return the url which redirects and returns the user list without the user that was deleted
+     */
     @GetMapping("/user/delete/{id}")
-    public String deleteUser(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        userRepository.delete(user);
-        model.addAttribute("users", userRepository.findAll());
-        return "redirect:/user/list";
+    public String deleteUser(@AuthenticationPrincipal UserDetails username, @PathVariable("id") Integer id, Model model) {
+        User userById = userService.findUserById(id);
+
+        if(userById != null){
+            log.info("User to delete: " + id);
+            try{
+                userService.deleteUser(id);
+                model.addAttribute("users", userService.findAllUsers());
+            }catch (Exception e){
+                System.out.println("error with deleting user");
+            }
+            log.info("Deleting User: " + userById.toString());
+            return "redirect:/user/list";
+        }
+        return "user/list";
     }
 
 }
