3995a436b6905470092ae43eb0a5233cb839e5dc
chyacin <calvinhyacinth@gmail.com>
2021-01-21 16:01:13 +0100

    Added methods to the User service class and added the javadocs

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserService.java
@@ -2,8 +2,15 @@ package com.nnk.springboot.services;
 
 import com.nnk.springboot.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findUserByUsername(String username);
+    public User saveUser(User user);
+    public User findUserById(int id);
+    List<User> findAllUsers();
+    public void updateUser(User user);
+    public void deleteUser(int id);
 
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserServiceImpl.java
@@ -4,7 +4,11 @@ package com.nnk.springboot.services;
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -12,9 +16,81 @@ public class UserServiceImpl implements UserService{
     @Autowired
     UserRepository userRepository;
 
+
+
+    /**
+     * The service method which finds the User by their username
+     * @param username the username to be found
+     * @return the found username object
+     */
     @Override
     public User findUserByUsername(String username) {
         return userRepository.findUserByUsername(username);
+    }
+
+    /**
+     * The service method which saves user to the database
+     * @param user to be saved
+     * @return the saved user object
+     */
+    @Override
+    public User saveUser(User user) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    /**
+     * The service method which finds the respective user by their corresponding id
+     * @param id of the user
+     * @return the found user
+     */
+    @Override
+    public User findUserById(int id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()){
+            User user = userOptional.get();
+            return user;
+        }
+        return null;
+    }
+
+    /**
+     * The service method which finds and returns all the users in a list
+     * @return the list of all the users
+     */
+    @Override
+    public List<User> findAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users;
+    }
+
+    /**
+     *  The service method which updates the user's details
+     * @param user to be updated
+     */
+    @Override
+    public void updateUser(User user) {
+        User updateUser = findUserById(user.getId());
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if(updateUser != null){
+            updateUser.setFullname(user.getFullname());
+            updateUser.setUsername(user.getUsername());
+            updateUser.setPassword(encoder.encode(user.getPassword()));
+            updateUser.setRole(user.getRole());
+            userRepository.save(updateUser);
+        }
+
+    }
+
+    /**
+     * The service method which deletes the user's details
+     * @param id of the user
+     */
+    @Override
+    public void deleteUser(int id) {
+      userRepository.deleteById(id);
+
     }
 
 
